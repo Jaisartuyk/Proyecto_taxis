5d05a46f118673c41f10136ebf4c4eb3c48311eb
Jaisartuyk <jairosancheztriana@gmail.com>
2025-04-28 00:07:58 -0500

    Añadir daphne a requirements.txt

--- taxi_project/wsgi.py
+++ taxi_project/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os
+# taxi_project/asgi.py
 
-from django.core.wsgi import get_wsgi_application
+import os
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taxi_project.settings')
 
-application = get_wsgi_application()
+application = get_asgi_application()
+

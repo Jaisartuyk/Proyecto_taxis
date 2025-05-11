87ff215806058c60a4f82127de87440095c099b4
Jaisartuyk <jairosancheztriana@gmail.com>
2025-05-10 21:58:51 -0500

    fix: added railway domain to ALLOWED_HOSTS

--- taxi_project/settings.py
+++ taxi_project/settings.py
@@ -165,7 +165,8 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get("REDIS_URL", "redis://default:PmdhUmFVXhjnezMlExGTbnBpitccmaPr@redis.railway.internal:6379")],
+            "hosts": [os.environ.get("REDIS_URL", "redis://default:AXvhAAIjcDEzMmZmMGExYmI2NDI0MzIyYWNjYmQyZmQwMmRlMmU0N3AxMA@intense-walrus-31713.upstash.io:6379")],
+            "ssl_cert_reqs": None,
         },
     },
 }

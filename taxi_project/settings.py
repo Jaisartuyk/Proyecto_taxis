834e2bd066e9ba5c634f9ffe98f8ea0e691e7808
Jaisartuyk <jairosancheztriana@gmail.com>
2025-05-10 20:58:18 -0500

    fix: added railway domain to ALLOWED_HOSTS

--- taxi_project/settings.py
+++ taxi_project/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = 'django-insecure-&)1eh^(=g#60plv-&xu$@@ngqr9lm$qf62*_(niv+k@d2*aw!_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'taxis-django-channels-production.up.railway.app',
+    'localhost',
+    '127.0.0.1',
+]
 
 # settings.py
 CSRF_TRUSTED_ORIGINS = ['https://proyecto-django-channels.onrender.com']

86c9390bec901bab8b28c07ac8bf805600bf2161
Jaisartuyk <jairosancheztriana@gmail.com>
2025-06-14 23:00:34 -0500

    Conecto

--- taxi_project/settings.py
+++ taxi_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&)1eh^(=g#60plv-&xu$@@ngqr9lm$qf62*_(niv+k@d2*aw!_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'taxis-deaquipalla.up.railway.app',

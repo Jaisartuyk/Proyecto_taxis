3c73cdb8019973fa3b650c24c35f9468ec92c974
Jaisartuyk <jairosancheztriana@gmail.com>
2025-10-10 13:06:48 -0500

    Conecto

--- taxi_project/settings.py
+++ taxi_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -96,10 +97,11 @@ WSGI_APPLICATION = 'taxi_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+        default=f'sqlite:///{BASE_DIR / 'db.sqlite3'}'
+    )
 }
 
 

15b6c199e47480881fc35299a97804797442bcc0
javier c1 <jaisa@localhost.localdomain>
2025-12-20 10:48:34 -0500

    fix: Add static/ directory to STATICFILES_DIRS
    - Django now searches in both static/ and taxis/static/
    - Fixes 404 for audio-floating-button.js and floating-audio-button.css
    - collectstatic will now copy files from both directories

--- taxi_project/settings.py
+++ taxi_project/settings.py
@@ -200,6 +200,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # carpeta destino para producción
 STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Archivos estáticos globales
     os.path.join(BASE_DIR, 'taxis', 'static'),  # Archivos estáticos de la app taxis
 ]
 
